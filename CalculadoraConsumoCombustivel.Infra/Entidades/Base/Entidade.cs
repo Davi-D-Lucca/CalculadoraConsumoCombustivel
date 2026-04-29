ee0c335c76236b847d17016cdca313deba80f6ef
Davi-D-Lucca <ddlucca10@gmail.com>
2026-04-29 11:09:02 -0300

    create entidade

---
+++ CalculadoraConsumoCombustivel.Infra/Entidades/Base/Entidade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraConsumoCombustivel.Infra.Entidades.Base
+{
+    class Entidade
+    {
+        //Travado em inteiro o projeto inteiro
+        public int Id { get; set; }
+        public DateTime CriadoEm { get; set; }
+        public DateTime? ModificadoEm { get; set; }
+    }
+}
---
+++ CalculadoraConsumoCombustivel.Infra/Entidades/MediaConsumoCombustivel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculadoraConsumoCombustivel.Infra.Entidades
+{
+    public class MediaConsumoCombustivel
+    {
+        public string Veiculo { get; set; }
+        public TipoCombustivel Combustivel { get; set; }
+        public int Quantidade { get; set; }
+        public int ConsumoMedio { get; set; }
+        public string DistanciaPercorrida { get; set; }
+    }
+}
